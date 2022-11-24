5a1e798b2fda6f7c8c3d516a874f50e5c124384b
CheloCarbone <105830357+CheloCarbone@users.noreply.github.com>
2022-11-23 21:02:53 -0300

    cambio de imagenes
    cambio de imagenes y textos

--- src/imagencondesplegable/acordeon/index.js
+++ src/imagencondesplegable/acordeon/index.js
@@ -7,39 +7,21 @@ function FlushExample() {
 
     <Accordion defaultActiveKey="0" flush>
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Header>Recuperar a las especies clave</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Este año, el Día Mundial de la Vida Silvestre se celebra bajo el tema: "Recuperar a las especies clave para la restauración de ecosistemas" con el fin de crear conciencia sobre el estado de las especies vida silvestre en peligro y en peligro crítico y resaltar el poder de los esfuerzos de conservación que están en marcha para alterar su destino.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Header>El incalculable valor de la vida silvestre</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        El Día Mundial de la Vida Silvestre nos brinda la ocasión de celebrar la belleza y la variedad de la flora y la fauna salvajes, así como de crear conciencia acerca de la multitud de beneficios que la conservación de estas formas de vida tiene para la humanidad.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="3">
-        <Accordion.Header>Accordion Item #3</Accordion.Header>
+        <Accordion.Header>¿Cómo puedo participar?</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Descubre más cosas sobre las especies animales y vegetales amenazadas, sus características y hábitats, las amenazas a las que se enfrentan y averigua cómo puedes contribuir a su conservación.
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
--- src/texto/texto.js
+++ src/texto/texto.js
@@ -16,9 +16,9 @@ function AutoLayoutExample() {
         <Col>
       <div class="intro">
         <div class="inner">
-            <h2 class="wb"> LOREM LOREM</h2>
-            <h2 class="wb2">LOREM LOREM</h2>
-            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 class="wb"> LA VIDA</h2>
+            <h2 class="wb2">SILVESTRE</h2>
+            <p> Los animales salvajes y las plantas silvestres, además de su valor intrínseco, contribuyen a los aspectos ecológicos, genéticos, sociales, económicos, científicos, educativos, culturales, recreativos y estéticos del bienestar humano y el desarrollo sostenible.</p>
         </div>
       </div>
       </Col>
