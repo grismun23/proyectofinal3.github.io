36cb90df698bfad0cd6e0a945082adca35b633a1
grismun23 <griseldajaquelinemunizdominguez@MacBook-Air-de-Griselda.local>
2022-11-28 22:09:05 -0600

    slider nuevas fotos

--- src/slider/index.js
+++ src/slider/index.js
@@ -13,14 +13,14 @@ function CarouselFadeExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://pixabay.com/get/g980c6592d53f913d6dfb200aca383942901d8de4e4c22cf49283c960192498318eb6e8f845272c0f00c9c25966dacf435efb4077d765af0448590ef55e1af56d_1280.jpg"
+          src="https://cdn.pixabay.com/photo/2022/11/12/16/42/elk-7587415_1280.jpg"
           alt="Second slide"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://pixabay.com/get/g9fdbc92921d48c26926834c25d57fda1f9ba6dc9a4f64fb207355ef447ba19fe3368e7ad272b72b302592995ee076a81b277111bf72200e90a298fec519c455b_1280.jpg"
+          src="https://images.unsplash.com/photo-1477949775154-d739b82400b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1174&q=80"
           alt="Third slide"
         />
       </Carousel.Item>
